311fd67bb5cbee3064a169902a7642b24aafe6e3
Cowme <codewithme2k@gmail.com>
2024-09-16 23:14:09 +0700

    changes color bg

--- src/app/(auth)/auth/layout.tsx
+++ src/app/(auth)/auth/layout.tsx
@@ -3,7 +3,7 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     <div
       className="
           min-h-screen flex items-center justify-center
-          bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-neutral-900 via-neutral-600 to-neutral-900
+          bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-yellow-100 bg-green-300 to-orange-200
         "
     >
       {children}
